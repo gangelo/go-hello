5a9858c61310c5cfd0af90cd0d3f968a89c1011d
gangelo <web.gma@gmail.com>
2025-02-23 18:32:44 -0500

    Implement dynamic greeting
    Using the all the methods module `quote` has to offer,
    and some other fun stuff.

--- hello.go
+++ hello.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
 	"rsc.io/quote"
 )
 
+var defaultGreetings = []string{
+	quote.Glass(),
+	quote.Go(),
+	quote.Hello(),
+	quote.Opt(),
+}
+
 func main() {
-		fmt.Println(quote.Go())
+	fmt.Println(Greeting())
+}
+
+func Greeting() string {
+	return formattedTime() + ": " + randomGreeting()
+}
+
+func formattedTime() string {
+	return time.Now().Format(time.RFC822)
+}
+
+func randomGreeting() string {
+	return defaultGreetings[rand.Intn(len(defaultGreetings))]
 }
